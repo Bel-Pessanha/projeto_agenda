1d7fd99bd6f15993a26d40b4ddcef2a1763f6155
Gabriel <34463768+Bel-Pessanha@users.noreply.github.com>
2025-06-19 21:22:31 -0300

    atualização

--- sql/projeto_agenda.sql
+++ sql/projeto_agenda.sql
@@ -11,5 +11,19 @@ datanasc date
 );
 
 select * from usuario where email = 'asdf@asdf' and senha = '123456';
-
 insert into  usuario values (default,'biel@biel','123456','henrique','giovani','2000-12-11');
+
+create table anotacao(
+id int primary key auto_increment,
+titulo varchar(255),
+texto varchar(255) not null,
+data_informacao date,
+criador int references usuario(id)
+);
+
+insert into anotacao values (default,'teste','testando este sql','2025-06-24','2');
+
+
+select * from anotacao;
+
+drop table anotacao;
