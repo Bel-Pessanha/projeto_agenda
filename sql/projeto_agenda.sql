84436a78fd87e8be8691a9c3ed890f78b7dfa195
Gabriel <34463768+Bel-Pessanha@users.noreply.github.com>
2025-07-13 00:29:28 -0300

    Update projeto_agenda.sql

--- sql/projeto_agenda.sql
+++ sql/projeto_agenda.sql
@@ -11,23 +11,41 @@ datanasc date
 );
 
 select * from usuario where email = 'asdf@asdf' and senha = '123456';
-insert into  usuario values (default,'biel@biel','123456','henrique','giovani','2000-12-11');
+insert into  usuario values (default,'biel2@biel','123456','henrique','giovani','2000-12-11');
 select * from usuario where id = '';
+ALTER TABLE usuario ADD compartilhado varchar(255);
+insert into usuario (compartilhado) values ('99') where id ='2';
 
 create table anotacao(
 id int primary key auto_increment,
 titulo varchar(255),
 texto varchar(255) not null,
 data_informacao date,
+compartilha varchar(255),
 criador int references usuario(id)
 );
 
-insert into anotacao values (default,'teste','testando este sql','2025-06-24','2');
-select * from anotacao where criador = '2' and id = '6';
-DELETE FROM anotacao WHERE id = '2' AND criador = '4';
+ALTER TABLE anotacao ADD compartilha varchar(255);
 
+insert into anotacao values (default,'teste','testando este sql','2025-06-24','2');
+
+SELECT id FROM anotacao where criador ='2' ORDER BY id DESC LIMIT 1 ;
+select * from anotacao where criador = '2' and id = '6';
 select * from anotacao;
+select * from usuario where email ='biel@biel';
 
 UPDATE anotacao SET titulo = 'teste', texto = 'teste', data_informacao = '2025-06-29' WHERE id = '3';
 
-drop table anotacao;
+DELETE FROM anotacao WHERE id = '2' AND criador = '4';
+
+drop table vinculo;
+
+
+create table vinculo(
+id int primary key auto_increment,
+criador_anot int,
+compa_anot int,
+anota_vinculo int
+);
+select * from vinculo;
+insert into vinculo values (default,'2','4','3');
